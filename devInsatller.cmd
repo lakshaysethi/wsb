21ff4f30fba3ee79c3825e58af4edcb7a75287f7
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-02-14 09:33:00 +1300

    updated

--- devInsatller.cmd
+++ devInsatller.cmd
@@ -1,33 +1,14 @@
-
+REM  Visual Studio Code
+curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Downloads\vscode.exe
+C:\users\WDAGUtilityAccount\Downloads\vscode.exe /verysilent /suppressmsgboxes
 
 REM curl -L "down-link" --output C:\users\WDAGUtilityAccount\Downloads\prog.exe
 REM C:\users\WDAGUtilityAccount\Downloads\prog.exe /verysilent /suppressmsgboxes
-
-REM 7 zip
-
-curl -L "https://www.7-zip.org/a/7z1900.exe" --output C:\users\WDAGUtilityAccount\Downloads\7-zip.exe
-C:\users\WDAGUtilityAccount\Downloads\7-zip.exe  /S /verysilent /suppressmsgboxes
-
-REM idm
-
-:: curl -L "https://mirror2.internetdownloadmanager.com/idman638build16.exe" --output C:\users\WDAGUtilityAccount\Downloads\idm.exe
-copy "C:\Users\WDAGUtilityAccount\Desktop\wsb\idm.exe" "C:\users\WDAGUtilityAccount\Downloads\"
-C:\users\WDAGUtilityAccount\Downloads\idm.exe  /skipdlgs /quiet /S /verysilent /suppressmsgboxes
-
-REM filezilla
-
-curl -L "https://download.filezilla-project.org/client/FileZilla_3.52.2_win64-setup.exe" --output C:\users\WDAGUtilityAccount\Downloads\FileZilla_3.52.2_win64-setup.exe
-C:\users\WDAGUtilityAccount\Downloads\FileZilla_3.52.2_win64-setup.exe /quiet /S /verysilent /suppressmsgboxes
 
 REM firefox
 
 curl -L "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=en-US" --output C:\users\WDAGUtilityAccount\Downloads\firefox.exe
 C:\users\WDAGUtilityAccount\Downloads\firefox.exe /quiet /S /verysilent /suppressmsgboxes
-
-
-REM  Visual Studio Code
-curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Downloads\vscode.exe
-C:\users\WDAGUtilityAccount\Downloads\vscode.exe /verysilent /suppressmsgboxes
 
 REM github Desktop
 
@@ -48,4 +29,19 @@ REM nodejs
 curl -L "https://nodejs.org/dist/v14.15.4/node-v14.15.4-x64.msi" --output C:\users\WDAGUtilityAccount\Downloads\node-v14.15.4-x64.msi
 C:\users\WDAGUtilityAccount\Downloads\node-v14.15.4-x64.msi /quiet 
 
+REM 7 zip
+
+curl -L "https://www.7-zip.org/a/7z1900.exe" --output C:\users\WDAGUtilityAccount\Downloads\7-zip.exe
+C:\users\WDAGUtilityAccount\Downloads\7-zip.exe  /S /verysilent /suppressmsgboxes
+
+REM idm
+
+:: curl -L "https://mirror2.internetdownloadmanager.com/idman638build16.exe" --output C:\users\WDAGUtilityAccount\Downloads\idm.exe
+copy "C:\Users\WDAGUtilityAccount\Desktop\wsb\idm.exe" "C:\users\WDAGUtilityAccount\Downloads\"
+C:\users\WDAGUtilityAccount\Downloads\idm.exe  /skipdlgs /quiet /S /verysilent /suppressmsgboxes
+
+REM filezilla
+
+curl -L "https://download.filezilla-project.org/client/FileZilla_3.52.2_win64-setup.exe" --output C:\users\WDAGUtilityAccount\Downloads\FileZilla_3.52.2_win64-setup.exe
+C:\users\WDAGUtilityAccount\Downloads\FileZilla_3.52.2_win64-setup.exe /quiet /S /verysilent /suppressmsgboxes
 
