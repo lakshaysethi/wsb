1f5cf0ad8cd6e985a50716ba694637528e056596
unknown <45325278+lakshaysethi@users.noreply.github.com>
2021-05-21 15:36:51 +1200

    add chrome support

--- programsInstall.cmd
+++ programsInstall.cmd
@@ -1,2 +1,6 @@
 copy "C:\Users\WDAGUtilityAccount\Desktop\wsb\idm.exe" "C:\users\WDAGUtilityAccount\Downloads\"
 C:\users\WDAGUtilityAccount\Downloads\idm.exe  /skipdlgs /quiet /S /verysilent /suppressmsgboxes
+
+
+copy "C:\Users\WDAGUtilityAccount\Desktop\wsb\ChromeSetup.exe" "C:\users\WDAGUtilityAccount\Downloads\"
+C:\users\WDAGUtilityAccount\Downloads\ChromeSetup.exe 
